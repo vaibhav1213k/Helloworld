21431c4892f70b2e2d8a383faf165196171e2d0a
Vaibhav Raju Konnur <VA20587219@wipro.com>
2026-01-28 10:08:41 +0530

    second commit

--- HelloWorld2.java
+++ HelloWorld2.java
@@ -3,5 +3,10 @@ public class HelloWorld2
     public static void main(String[] args){
 
         System.out.println("Hello world");
-    }
+
+        for(int i = 0; i <= 5; i++){
+            System.out.println("HEllo world");
+        }
+    } 
+    // second commit
 }
